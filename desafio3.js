071ee74e27631d239930c1f27a4aa385e8b3f219
antoniocarlosff <antonio.carlosff@hotmail.com>
2024-01-19 20:18:24 -0300

    modif

---
+++ desafio3.js
@@ -0,0 +1,58 @@
+/*
+## Objetivo:
+Crie uma classe generica que represente um herói de uma aventura e que possua as seguintes propriedades:
+*/
+
+//Construir Heroi
+class clHeroi {
+
+    constructor (HeroiNOME, HeroiIDADE, HeroiTIPO) {
+        this.heroiNome = HeroiNOME;
+        this.heroiIDADE = HeroiIDADE;
+        this.heroiTIPO = HeroiTIPO;
+    }
+
+    //método para definir tipo de ataque
+    getTipoAtaque() {
+
+        var ataqueTIPO;
+
+        switch (this.heroiTIPO) {
+            case 'mago':
+                ataqueTIPO = 'magia';
+                break;
+
+            case 'guerreiro':
+                ataqueTIPO = 'espada';
+                break;
+
+            case 'monge':
+                ataqueTIPO = 'artes marciais';
+                break;
+
+            case 'ninja':
+                ataqueTIPO = 'shuriken';
+                break;
+        }
+
+        return ataqueTIPO;
+    }
+
+    //método atacar
+    moveATACAR() {
+        var moveATAQUE = this.getTipoAtaque();
+        console.log(`O ${this.heroiTIPO} atacou usando ${moveATAQUE}`);
+    }
+}
+
+//Cria party
+let prtHEROI1 = new clHeroi('Merlin',99,'mago');
+let prtHEROI2 = new clHeroi('Conan',41,'guerreiro');
+let prtHEROI3 = new clHeroi('JohnPaul',33,'monge');
+let prtHEROI4 = new clHeroi('Gaiden',30,'ninja');
+
+//Atacar
+prtHEROI1.moveATACAR();
+prtHEROI2.moveATACAR();
+prtHEROI3.moveATACAR();
+prtHEROI4.moveATACAR();
